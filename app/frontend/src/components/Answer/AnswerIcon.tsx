8190bf03111459318c818731f35ef36fc072129b
Immo Garlichs <immo.garlichs@breakingwave.io>
2023-07-14 11:21:46 +0200

    Add logo to answer

--- app/frontend/src/components/Answer/AnswerIcon.tsx
+++ app/frontend/src/components/Answer/AnswerIcon.tsx
@@ -1,5 +1,6 @@
 import { Sparkle28Filled } from "@fluentui/react-icons";
+import logo from '../../static/dblogo.png';
 
 export const AnswerIcon = () => {
-    return <Sparkle28Filled primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Answer logo" />;
+    return logo;
 };
